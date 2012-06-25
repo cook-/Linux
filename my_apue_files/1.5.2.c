bb6628943303a06c400e581970f54762426e9953
cook <yuanxiaojie6616@163.com>
2012-06-25 17:11:43 +0800

    + ‘my_apue_files/1.5.2.c’

---
+++ my_apue_files/1.5.2.c
@@ -0,0 +1,18 @@
+/*
+ *copy standard input to standard output, using standard I/O.
+ */
+#include "apue.h"
+int
+main(void)
+{
+	int c;
+
+	while((c = getc(stdin)) != EOF)
+		if (putc(c, stdout) == EOF)
+			err_sys("output error");
+
+	if (ferror(stdin))
+		err_sys("input error");
+
+	exit(0);
+}
