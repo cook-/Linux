e57b2eb19cc1617127373c6b2c3c7d7c2dce457e
cook <yuanxiaojie6616@163.com>
2012-07-03 20:26:47 +0800

    + ‘Chapter1/7_1.c’

---
+++ Chapter1/7_1.c
@@ -0,0 +1,15 @@
+/*
+ * demonstrate strerror and perror.
+ */
+
+#include "apue.h"
+#include <errno.h>
+
+int
+main(int argc, char *argv[])
+{
+	fprintf(stderr, "EACCES: %s/n", strerr(EACCES));
+	errno = ENOENT;
+	perror(argv[0]);
+	exit(0);
+}
