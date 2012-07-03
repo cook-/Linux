0bf26ba070007342f980acd38d6400bf964e35c7
cook <yuanxiaojie6616@163.com>
2012-07-03 21:14:32 +0800

    / ‘Chapter1/9.c’

--- Chapter1/9.c
+++ Chapter1/9.c
@@ -1,7 +1,47 @@
 /*
  * read commands from standard input and execute them.
- * a more complex version of .c
+ * a more complex version of 6_2.c.
  */
 
 #include "apue"
 #include <sys/wait.h>
+
+static void sig_int(int);	/* our signal-catching function*/
+
+int
+main(void)
+{
+	char	buf[MAXLINE];	/* from apue.h */
+	pid_t	pid;
+	int		status;
+
+	if (signal(SIGINT, sig_int) == SIG_ERR)
+		errsys("signal error");
+
+	printf("%% ");	/* print prompt (printf requires %% to print %) */
+	while (fget(buf, MAXLINE, stdin) != NULL) {
+		if (buf[strlen(buf) - 1] == "\n")
+			buf[strlen(buf) - 1] = 0;	/* replace a newline with a null */
+
+		if ((pid = fork()) < 0) {
+			err_sys("fork error");
+		}
+		else if (pid == 0) {
+			execlp(buf, buf, (char *)0);
+			err_ret("couldn't execute: %s", buf);
+			exit(127);
+		}
+
+		/* parent */
+		if ((pid = waitpid(pid, &status, 0)) < 0)
+			err_sys("waitpid error");
+		printf("%% ");
+	}
+	exit(0);
+}
+
+void
+sig_int(int signo)
+{
+	printf("interrupt\n%%");
+}
