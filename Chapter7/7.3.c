74c6738002a7c473ae1f15431edca9d2c7ffe23b
cook <yuanxiaojie6616@163.com>
2012-09-02 07:50:34 +0800

    / ‘Chapter7/7.3.c’

--- Chapter7/7.3.c
+++ Chapter7/7.3.c
@@ -2,6 +2,7 @@
 
 static void my_exit1(void);
 static void my_exit2(void);
+static void my_exit3(void);
 
 int
 main(void)
@@ -12,8 +13,8 @@ main(void)
 	if (atexit(my_exit1) != 0)
 		err_sys("can't register my_exit1");
 
-	if (atexit(my_exit1) != 0)
-		err_sys("can't register my_exit1");
+	if (atexit(my_exit3) != 0)
+		err_sys("can't register my_exit3");
 
 	printf("main is done\n");
 	return(0);
@@ -29,4 +30,10 @@ static void
 my_exit2(void)
 {
 	printf("second exit handler\n");
+}
+
+static void
+my_exit3(void)
+{
+	printf("third exit handler\n");
 }
