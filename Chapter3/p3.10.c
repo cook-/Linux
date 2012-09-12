58f5670c9d26c2cad8a0bd56b65123d5a66e182c
cook <yuanxiaojie6616@163.com>
2012-09-12 22:19:20 +0800

    / ‘Chapter3/a.out’ + ‘Chapter3/p3.10.c’

---
+++ Chapter3/p3.10.c
@@ -0,0 +1,48 @@
+/*
+ * Print file flags for specified descriptor.
+ */
+
+#include "apue.h"
+#include <fcntl.h>
+
+int
+main(int argc, char *argv[])
+{
+	int val;
+	if (argc != 2)
+		err_quit("Usage: ./a.out <descriptor#>");
+	if ((val = fcntl(atoi(argv[1]), F_GETFL, 0)) < 0)
+		err_sys("fctnl error for fd %d", atoi(argv[1]));
+
+	switch (val & O_ACCMODE) {
+	case O_RDONLY:
+		printf("read only");
+		break;
+	case O_WRONLY:
+		printf("write only");
+		break;
+	case O_RDWR:
+		printf("read write");
+	default:
+		err_dump("unknown access mode");
+	}
+
+	if (val & O_APPEND)
+		printf(", append");
+	if (val & O_NONBLOCK)
+		printf(", nonblocking");
+
+#if defined(O_SYNC)
+	if (val & O_SYNC)
+		printf(", synchronous writes");
+#endif
+
+#if !defined(_POSIX_C_SOURCE) && defined(O_FSYNC)
+	if (val & O_FSYNC)
+		printf(", synchronous writes");
+#endif
+
+	putchar('\n');
+
+	exit(0);
+}
